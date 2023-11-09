6d30ae69d21737ffc68972063c1a20978938a243
Daniil <dankon368024@gmail.com>
2023-11-09 21:37:30 +0300

    edited resource-manifests

--- sa-frontend/src/App.js
+++ sa-frontend/src/App.js
@@ -20,7 +20,7 @@ class App extends Component {
     };
 
     analyzeSentence() {
-        fetch('http://192.168.49.2:32214/sentiment', {
+        fetch('http://192.168.49.2:32194/sentiment', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
